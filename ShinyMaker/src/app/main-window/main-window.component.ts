bbfb9b66b003687bd1e16c926e7fddedcb291085
YSRKEN <ysr.ken@gmail.com>
2018-12-06 00:32:09 +0900

    画像保存機能を追加した

--- ShinyMaker/src/app/main-window/main-window.component.ts
+++ ShinyMaker/src/app/main-window/main-window.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-main-window',
@@ -190,7 +191,54 @@ export class MainWindowComponent implements OnInit {
     if (this.talkData.length == 0) {
       return;
     }
+    html2canvas(document.querySelector("#capture"), {
+      allowTaint: true,
+      useCORS: true,
+      proxy: true,
+      onrendered: function (canvas) {
+        canvas.toDataURL();
+      }
+    }).then(canvas => {
+      // Base64データに変換
+      const base64 = canvas.toDataURL();
 
+      // blobに変換
+      const blob = this.Base64toBlob(base64);
+
+      // ファイルの保存イベントを走らせる
+      this.saveBlob(blob, "shiny_maker.png");
+    });
+  }
+
+  /**
+   * Base64をblobに変換する
+   * @param base64 Base64
+   */
+  Base64toBlob(base64): Blob {
+    var tmp = base64.split(',');
+    var data = atob(tmp[1]);
+    var mime = tmp[0].split(':')[1].split(';')[0];
+    var buf = new Uint8Array(data.length);
+    for (var i = 0; i < data.length; i++) {
+      buf[i] = data.charCodeAt(i);
+    }
+    var blob = new Blob([buf], { type: mime });
+    return blob;
+  }
+
+  /**
+   * blobを保存する
+   * @param blob 
+   */
+  saveBlob(blob: Blob, fileName: string){
+    const url = window.URL;
+    const dataUrl = url.createObjectURL(blob);
+    const event = document.createEvent("MouseEvents");
+    event.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    const a = document.createElementNS("http://www.w3.org/1999/xhtml", "a");
+    (a as HTMLAnchorElement).href = dataUrl;
+    (a as HTMLAnchorElement).download = fileName;
+    a.dispatchEvent(event);
   }
 
   /**
