c78f79fea5008f8d8f66dffd7fc4ac91ea9b74ec
YSRKEN <ysr.ken@gmail.com>
2020-10-30 17:40:35 +0900

    [add] 入力フォームを仮作成した

--- shiny-talk-maker/src/App.tsx
+++ shiny-talk-maker/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row, Button} from 'react-bootstrap';
+
+// 仮実装
+const Preview: React.FC = () => <div className="border w-100" style={{height: 100}}></div>;
 
 const App: React.FC = () => (
     <Container>
@@ -13,6 +16,30 @@ const App: React.FC = () => (
         <Col className="text-center">
           <span>Ver.2.0.0　<a href="https://github.com/YSRKEN/shiny_maker" rel="noreferrer" target="_blank">GitHub</a>
           　<a href="https://twitter.com/YSRKEN" rel="noreferrer" target="_blank">作者のTwitter</a></span>
+        </Col>
+      </Row>
+      <Row className="my-3">
+        <Col>
+          <Form className="border px-3 pt-3">
+            <Form.Group>
+              <Form.Label>キャラ名</Form.Label>
+              <Form.Control as="select">
+
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>発言</Form.Label>
+              <Form.Control as="textarea" rows={3}>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>プレビュー</Form.Label>
+              <Preview />
+            </Form.Group>
+            <Form.Group>
+              <Button className="w-100">追加</Button>
+            </Form.Group>
+          </Form>
         </Col>
       </Row>
     </Container>
