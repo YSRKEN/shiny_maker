6b3d89ffa3a9d5ced5760ebaa73bba638a838e33
YSRKEN <ysr.ken@gmail.com>
2020-10-30 13:49:42 +0900

    [add] BootstrapおよびReact-Bootstrapをインストールした

--- shiny-talk-maker/src/App.tsx
+++ shiny-talk-maker/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+    <Container>
+      <Row>
+        <Col>
+          <h2>シャニマス会話メーカー</h2>
+        </Col>
+      </Row>
+    </Container>
+);
 
 export default App;
