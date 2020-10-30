ae331ffcd22458a31fe1ab8ececd0e160be1ee68
YSRKEN <ysr.ken@gmail.com>
2020-10-30 14:03:17 +0900

    [add] アプリ名部分の実装を移植した

--- shiny-talk-maker/src/App.tsx
+++ shiny-talk-maker/src/App.tsx
@@ -3,9 +3,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 const App: React.FC = () => (
     <Container>
-      <Row>
-        <Col>
-          <h2>シャニマス会話メーカー</h2>
+      <Row className="my-3">
+        <Col className="text-center">
+          <h1 className="d-none d-sm-inline">シャニマス会話メーカー</h1>
+          <h3 className="d-inline d-sm-none">シャニマス会話メーカー</h3>
+        </Col>
+      </Row>
+      <Row className="my-3">
+        <Col className="text-center">
+          <span>Ver.2.0.0　<a href="https://github.com/YSRKEN/shiny_maker" rel="noreferrer" target="_blank">GitHub</a>
+          　<a href="https://twitter.com/YSRKEN" rel="noreferrer" target="_blank">作者のTwitter</a></span>
         </Col>
       </Row>
     </Container>
