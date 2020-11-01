0c7d6653ea95a3c63bb4ca23094429577e632888
YSRKEN <ysr.ken@gmail.com>
2020-11-01 18:29:24 +0900

    [fix] アプリ情報部分でエンバグしていたので修正

--- shiny-talk-maker/src/component/ApplicationInfo.tsx
+++ shiny-talk-maker/src/component/ApplicationInfo.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 // アプリ情報表示
 const ApplicationInfo: React.FC = () => {
-  return <span>Ver.2.0.0
-  <a href="https://github.com/YSRKEN/shiny_maker" rel="noreferrer" target="_blank">GitHub</a>
-    <a href="https://twitter.com/YSRKEN" rel="noreferrer" target="_blank">作者のTwitter</a>
-  </span>;
+  return <>
+    <span className="d-inline-block mr-3">Ver.2.0.0</span>
+    <span className="d-inline-block mr-3"><a href="https://github.com/YSRKEN/shiny_maker" rel="noreferrer" target="_blank">GitHub</a> </span>
+    <span><a href="https://twitter.com/YSRKEN" rel="noreferrer" target="_blank">作者のTwitter</a></span>
+  </>
 };
 
 export default ApplicationInfo;
