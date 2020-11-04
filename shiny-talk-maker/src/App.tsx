8d713b69363a64ca6a3a46a6e97de4d94e9b67a8
YSRKEN <ysr.ken@gmail.com>
2020-11-04 14:32:05 +0900

    [add] 簡易的に、メッセージごとの名前とテキストを表示できることを確認した

--- shiny-talk-maker/src/App.tsx
+++ shiny-talk-maker/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
 
 import MainView from 'component/MainView';
 import { ApplicationContext, useStore } from 'service/store';
@@ -11,20 +10,6 @@ const App: React.FC = () => {
   return (
     <ApplicationContext.Provider value={store}>
       <MainView />
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="Try click on rect" />
-          <Rect
-            x={20}
-            y={20}
-            width={50}
-            height={50}
-            fill="green"
-            shadowBlur={5}
-            onClick={() => alert('OK!')}
-          />
-        </Layer>
-      </Stage>
     </ApplicationContext.Provider>
   );
 };
--- shiny-talk-maker/src/component/MessageView.tsx
+++ shiny-talk-maker/src/component/MessageView.tsx
@@ -1,21 +1,30 @@
 import React, { useContext } from 'react';
 import { Message } from 'model/Message';
 import { ApplicationContext } from 'service/store';
+import { Layer, Stage, Text } from 'react-konva';
 
 // メッセージ一覧
-const MessageView: React.FC<{messageList: Message[], startIndex?: number}> = ({messageList, startIndex=-1}) => {
+const MessageView: React.FC<{ messageList: Message[], startIndex?: number }> = ({ messageList, startIndex = -1 }) => {
   const { dispatch } = useContext(ApplicationContext);
 
-  const onClickMessageView = (index: number) => dispatch({type: 'setSplitIndex', message: `${startIndex + index}`});
+  const onClickMessageView = (index: number) => dispatch({ type: 'setSplitIndex', message: `${startIndex + index}` });
 
-  return <>
-    {messageList.map((message, index) =>
-    <div className="m-3 p-1 border" key={startIndex + index} onClick={() => onClickMessageView(index)}>
-      <span>{message.name}</span><br />
-      <pre>{message.talk}</pre>
-    </div>)
+  // JavaScriptで、表示サイズを決定
+  const width = Math.min(window.innerWidth, 850);
+  const height = 170 * width / 850;
+  const allHeight = height * messageList.length;
+  const nameFontSize = 30 * width / 850;
+  const talkFontSize = 24 * width / 850;
+
+  return <Stage className="border" width={width} height={allHeight}>
+    {
+      messageList.map((message, index) =>
+        <Layer key={index}>
+          <Text text={message.name} fontSize={nameFontSize} y={height * index} fontFamily="'Kosugi Maru', sans-serif" />
+          <Text text={message.talk} fontSize={talkFontSize} y={height * index + nameFontSize} fontFamily="'Kosugi Maru', sans-serif" />
+        </Layer>)
     }
-  </>;
+  </Stage>
 };
 
 export default MessageView;
